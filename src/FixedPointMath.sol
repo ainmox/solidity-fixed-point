f62db877cd9aa388a1f0e8dce1e52190f757001d
Ainmox <ainmo-x@protonmail.me>
2022-12-20 03:42:27 -0700

    feat: use 512 bit multiplication & division

--- src/FixedPointMath.sol
+++ src/FixedPointMath.sol
@@ -170,12 +170,11 @@ library FixedPointMath {
     /// @param y The second unsigned 256 bit 18 decimal fixed point number
     /// @return result The resulting unsigned 256 bit 18 decimal fixed point number
     function mul(UFixed256x18 x, UFixed256x18 y) internal pure returns (UFixed256x18 result) {
-        assembly {
-            if mul(mul(x, y), gt(x, div(MAX_UINT256, y))) {
-                revert(0, 0)
-            }
-        }
-        result = unsafeMul(x, y);
+        result = UFixed256x18.wrap(mulDiv(
+            UFixed256x18.unwrap(x),
+            UFixed256x18.unwrap(y),
+            X18_SCALING_FACTOR
+        ));
     }
 
     /// @dev Divides two unsigned 256 bit 18 decimal fixed point numbers
@@ -183,12 +182,11 @@ library FixedPointMath {
     /// @param y The unsigned 256 bit 18 decimal fixed point number to divide by
     /// @return result The resulting unsigned 256 bit 18 decimal fixed point number
     function div(UFixed256x18 x, UFixed256x18 y) internal pure returns (UFixed256x18 result) {
-        assembly {
-            if or(iszero(y), mul(x, gt(x, div(MAX_UINT256, X18_DIVISION_FACTOR)))) {
-                revert(0, 0)
-            }
-        }
-        result = unsafeDiv(x, y);
+        result = UFixed256x18.wrap(mulDiv(
+            UFixed256x18.unwrap(x),
+            X18_SCALING_FACTOR,
+            UFixed256x18.unwrap(y)
+        ));
     }
 
     /// @dev Adds two unsigned 256 bit 18 decimal fixed point numbers without checking for overflow
@@ -208,28 +206,6 @@ library FixedPointMath {
     function unsafeSub(UFixed256x18 x, UFixed256x18 y) internal pure returns (UFixed256x18 result) {
         assembly {
             result := sub(x, y)
-        }
-    }
-
-    /// @dev Multiplies two unsigned 256 bit 18 decimal fixed point numbers without checking for overflow
-    /// @param x The first unsigned 256 bit 18 decimal fixed point number
-    /// @param y The second unsigned 256 bit 18 decimal fixed point number
-    /// @return result The resulting unsigned 256 bit 18 decimal fixed point number
-    function unsafeMul(UFixed256x18 x, UFixed256x18 y) internal pure returns (UFixed256x18 result) {
-        assembly {
-            let z  := div(mul(x, y), X18_MULTIPLICATION_FACTOR)
-            result := div(add(z, mod(z, 10)), 10)
-        }
-    }
-
-    /// @dev Divides two unsigned 256 bit 18 decimal fixed point numbers without checking for division by zero or overflow
-    /// @param x The unsigned 256 bit 18 decimal fixed point number to divide
-    /// @param y The unsigned 256 bit 18 decimal fixed point number to divide by
-    /// @return result The resulting unsigned 256 bit 18 decimal fixed point number
-    function unsafeDiv(UFixed256x18 x, UFixed256x18 y) internal pure returns (UFixed256x18 result) {
-        assembly {
-            let z  := div(mul(x, X18_DIVISION_FACTOR), y)
-            result := div(add(z, mod(z, 10)), 10)
         }
     }
 
@@ -302,5 +278,110 @@ library FixedPointMath {
     /// @param result The unsigned 256 bit integer
     function unwrap(UFixed256x18 value) internal pure returns (uint256 result) {
         result = UFixed256x18.unwrap(value);
+    }
+
+    /// @dev Multiplies two unsigned 256 bit 18 decimal fixed point numbers and then divides them by an unsigned 256 bit
+    ///      integer while checking for overflow
+    /// @dev Taken from Remco Bloemen, see https://xn--2-umb.com/21/muldiv/. Licensed under MIT.
+    /// @param a The first unsigned 256 bit 18 decimal fixed point number
+    /// @param b The second unsigned 256 bit 18 decimal fixed point number
+    /// @param denominator The unsigned 256 bit integer to divide by
+    /// @return result The resulting unsigned 256 bit 18 decimal fixed point number
+    function mulDiv(uint256 a, uint256 b, uint256 denominator) private pure returns (uint256 result) {
+        // Handle division by zero
+        require(denominator > 0);
+
+        // 512-bit multiply [prod1 prod0] = a * b
+        // Compute the product mod 2**256 and mod 2**256 - 1
+        // then use the Chinese Remainder Theorem to reconstruct
+        // the 512 bit result. The result is stored in two 256
+        // variables such that product = prod1 * 2**256 + prod0
+        uint256 prod0; // Least significant 256 bits of the product
+        uint256 prod1; // Most significant 256 bits of the product
+        assembly {
+            let mm := mulmod(a, b, MAX_UINT256)
+            prod0 := mul(a, b)
+            prod1 := sub(sub(mm, prod0), lt(mm, prod0))
+        }
+
+        // Short circuit 256 by 256 division
+        // This saves gas when a * b is small, at the cost of making the
+        // large case a bit more expensive. Depending on your use case you
+        // may want to remove this short circuit and always go through the
+        // 512 bit path.
+        if (prod1 == 0) {
+            assembly {
+                result := div(prod0, denominator)
+            }
+            return result;
+        }
+
+        ///////////////////////////////////////////////
+        // 512 by 256 division.
+        ///////////////////////////////////////////////
+
+        // Handle overflow, the result must be < 2**256
+        require(prod1 < denominator);
+
+        // Make division exact by subtracting the remainder from [prod1 prod0]
+        // Compute remainder using mulmod
+        // Note mulmod(_, _, 0) == 0
+        uint256 remainder;
+        assembly {
+            remainder := mulmod(a, b, denominator)
+        }
+        // Subtract 256 bit number from 512 bit number
+        assembly {
+            prod1 := sub(prod1, gt(remainder, prod0))
+            prod0 := sub(prod0, remainder)
+        }
+
+        // Factor powers of two out of denominator
+        // Compute largest power of two divisor of denominator.
+        // Always >= 1 unless denominator is zero, then twos is zero.
+        uint256 twos = MAX_UINT256 - (denominator - 1) & denominator;
+        // Divide denominator by power of two
+        assembly {
+            denominator := div(denominator, twos)
+        }
+
+        // Divide [prod1 prod0] by the factors of two
+        assembly {
+            prod0 := div(prod0, twos)
+        }
+        // Shift in bits from prod1 into prod0. For this we need
+        // to flip `twos` such that it is 2**256 / twos.
+        // If twos is zero, then it becomes one
+        assembly {
+            twos := add(div(sub(0, twos), twos), 1)
+        }
+        prod0 |= prod1 * twos;
+
+        // Invert denominator mod 2**256
+        // Now that denominator is an odd number, it has an inverse
+        // modulo 2**256 such that denominator * inv = 1 mod 2**256.
+        // Compute the inverse by starting with a seed that is correct
+        // correct for four bits. That is, denominator * inv = 1 mod 2**4
+        // If denominator is zero the inverse starts with 2
+        uint256 inv = 3 * denominator ^ 2;
+        // Now use Newton-Raphson iteration to improve the precision.
+        // Thanks to Hensel's lifting lemma, this also works in modular
+        // arithmetic, doubling the correct bits in each step.
+        inv *= 2 - denominator * inv; // inverse mod 2**8
+        inv *= 2 - denominator * inv; // inverse mod 2**16
+        inv *= 2 - denominator * inv; // inverse mod 2**32
+        inv *= 2 - denominator * inv; // inverse mod 2**64
+        inv *= 2 - denominator * inv; // inverse mod 2**128
+        inv *= 2 - denominator * inv; // inverse mod 2**256
+        // If denominator is zero, inv is now 128
+
+        // Because the division is now exact we can divide by multiplying
+        // with the modular inverse of denominator. This will give us the
+        // correct result modulo 2**256. Since the precoditions guarantee
+        // that the outcome is less than 2**256, this is the final result.
+        // We don't need to compute the high bits of the result and prod1
+        // is no longer required.
+        result = prod0 * inv;
+        return result;
     }
 }
