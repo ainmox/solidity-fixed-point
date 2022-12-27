558ac7666c6e3358bf6142370f07dd0a9c56676e
Ainmox <ainmo-x@protonmail.me>
2022-12-27 02:37:48 -0700

    feat: add Fixed256x18

--- src/FixedPointMath.sol
+++ src/FixedPointMath.sol
@@ -25,6 +25,10 @@ type UFixed128x18 is uint128;
 /// @dev An unsigned 192 bit 18 decimal fixed point number
 type UFixed192x18 is uint192;
 
+/// @dev A signed 256 bit 18 decimal fixed point number
+/// @dev Commonly called WAD (https://github.com/makerdao/dss/blob/master/DEVELOPING.md#units)
+type Fixed256x18 is int256;
+
 /// @dev An unsigned 256 bit 4 decimal fixed point number
 /// @dev Minimum supported value for 4 decimal fixed point numbers
 type UFixed256x4 is uint256;
@@ -63,6 +67,8 @@ using FixedPointMath for UFixed112x18 global;
 using FixedPointMath for UFixed128x18 global;
 using FixedPointMath for UFixed192x18 global;
 using FixedPointMath for UFixed256x18 global;
+
+using FixedPointMath for Fixed256x18 global;
 
 /// @title Library for fixed point math operations
 /// @custom:coauthor ainmox (ainmox.eth)
@@ -221,6 +227,75 @@ library FixedPointMath {
         }
     }
 
+    /// @dev The natural exponentiation of a signed 256 bit 18 decimal fixed point number
+    /// @param x The signed 256 bit 18 decimal exponent
+    /// @return result The resulting signed 256 bit 18 decimal fixed point number
+    function exp(Fixed256x18 x) internal pure returns (Fixed256x18 result) {
+        result = Fixed256x18.wrap(exp(Fixed256x18.unwrap(x)));
+    }
+
+    /// @dev The natural exponentiation of a signed 256 bit 18 decimal fixed point number
+    /// @dev Taken from Remco Bloemen, see https://xn--2-umb.com/22/exp-ln/. Licensed under MIT.
+    /// @param x The 256 bit 18 decimal exponent
+    /// @return result The resulting signed 256 bit 18 decimal fixed point number
+    function exp(int256 x) private pure returns (int256 result) {
+        unchecked {
+            // When the result is < 0.5 we return zero. This happens when
+            // x <= floor(log(0.5e18) * 1e18) ~ -42e18
+            if (x <= -42139678854452767551) return 0;
+
+            // When the result is > (2**255 - 1) / 1e18 we can not represent it as an
+            // int. This happens when x >= floor(log((2**255 - 1) / 1e18) * 1e18) ~ 135.
+            if (x >= 135305999368893231589) revert("EXP_OVERFLOW");
+
+            // x is now in the range (-42, 136) * 1e18. Convert to (-42, 136) * 2**96
+            // for more intermediate precision and a binary basis. This base conversion
+            // is a multiplication by 1e18 / 2**96 = 5**18 / 2**78.
+            x = (x << 78) / 5**18;
+
+            // Reduce range of x to (-½ ln 2, ½ ln 2) * 2**96 by factoring out powers
+            // of two such that exp(x) = exp(x') * 2**k, where k is an integer.
+            // Solving this gives k = round(x / log(2)) and x' = x - k * log(2).
+            int256 k = ((x << 96) / 54916777467707473351141471128 + 2**95) >> 96;
+            x = x - k * 54916777467707473351141471128;
+
+            // k is in the range [-61, 195].
+
+            // Evaluate using a (6, 7)-term rational approximation.
+            // p is made monic, we'll multiply by a scale factor later.
+            int256 y = x + 1346386616545796478920950773328;
+            y = ((y * x) >> 96) + 57155421227552351082224309758442;
+            int256 p = y + x - 94201549194550492254356042504812;
+            p = ((p * y) >> 96) + 28719021644029726153956944680412240;
+            p = p * x + (4385272521454847904659076985693276 << 96);
+
+            // We leave p in 2**192 basis so we don't need to scale it back up for the division.
+            int256 q = x - 2855989394907223263936484059900;
+            q = ((q * x) >> 96) + 50020603652535783019961831881945;
+            q = ((q * x) >> 96) - 533845033583426703283633433725380;
+            q = ((q * x) >> 96) + 3604857256930695427073651918091429;
+            q = ((q * x) >> 96) - 14423608567350463180887372962807573;
+            q = ((q * x) >> 96) + 26449188498355588339934803723976023;
+
+            assembly {
+                // Div in assembly because solidity adds a zero check despite the unchecked.
+                // The q polynomial won't have zeros in the domain as all its roots are complex.
+                // No scaling is necessary because p is already 2**96 too large.
+                result := sdiv(p, q)
+            }
+
+            // result should be in the range (0.09, 0.25) * 2**96.
+
+            // We now need to multiply result by:
+            // * the scale factor s = ~6.031367120.
+            // * the 2**k factor from the range reduction.
+            // * the 1e18 / 2**96 factor for base conversion.
+            // We do this all at once, with an intermediate result in 2**213
+            // basis, so the final right shift is always by a positive amount.
+            result = int256((uint256(result) * 3822833074963236453042738258902158003155416615667) >> uint256(195 - k));
+        }
+    }
+
     /// @dev Rounds up an unsigned 256 bit 18 decimal fixed point number to the smallest unsigned 256 bit integer
     ///      greater than or equal to the number
     /// @param value The unsigned 256 bit 18 decimal fixed point number to round up
@@ -280,5 +355,13 @@ library FixedPointMath {
     /// @param result The unsigned 256 bit integer
     function unwrap(UFixed256x18 value) internal pure returns (uint256 result) {
         result = UFixed256x18.unwrap(value);
+    }
+
+    /// @dev Unwraps a signed 256 bit 18 decimal fixed point number into a signed 256 bit integer
+    /// @dev Alias for `Fixed256x18.unwrap`
+    /// @param value The signed 256 bit 18 decimal fixed point number to unwrap
+    /// @param result The signed 256 bit integer
+    function unwrap(Fixed256x18 value) internal pure returns (int256 result) {
+        result = Fixed256x18.unwrap(value);
     }
 }
